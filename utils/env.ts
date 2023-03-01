4132d9d1d710c1e4666cb51f0c76bf6d236c1b48
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-02-28 23:37:51 -0500

    chore: add base url environment variable (#2)

--- utils/env.ts
+++ utils/env.ts
@@ -1,22 +1,7 @@
-// https://vercel.com/docs/concepts/projects/environment-variables#reserved-environment-variables
-
-export const isDevelopment = (isServer = false): boolean => {
-  if (isServer) {
-    return process.env.VERCEL_ENV === 'development';
-  }
-  return process.env.NEXT_PUBLIC_VERCEL_ENV === 'development';
-};
-
-export const isProduction = (isServer = false): boolean => {
-  if (isServer) {
-    return process.env.VERCEL_ENV === 'production';
-  }
-  return process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
-};
-
 export const getBaseURL = (): string => {
-  if (isDevelopment()) {
-    return 'http://' + process.env.VERCEL_URL;
+  const url = process.env.BASE_URL || process.env.VERCEL_URL;
+  if (process.env.VERCEL_ENV === 'development') {
+    return 'http://' + url;
   }
-  return 'https://' + process.env.VERCEL_URL;
+  return 'https://' + url;
 };
