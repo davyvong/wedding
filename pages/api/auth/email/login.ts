cb7ac445008c33ab0c1403f398c1a32f53f52ddc
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-03-01 12:17:49 -0500

    refactor: remove server error codes

--- pages/api/auth/email/login.ts
+++ pages/api/auth/email/login.ts
@@ -1,4 +1,3 @@
-import serverErrors from 'constants/server-errors.json';
 import loginCodeTemplate from 'emails/login-code.eml';
 import * as handlebars from 'handlebars';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -11,36 +10,33 @@ import { getRandomWords } from 'utils/words';
 import { isEmail } from 'utils/yup';
 
 const handler = async (request: NextApiRequest, response: NextApiResponse): Promise<void> => {
-  try {
-    if (!isEmail(request.body.email)) {
-      throw new Error(serverErrors.invalidEmail);
-    }
-    const db = await getMongoDatabase();
-    const collection = db.collection('guests');
-    const doc = await collection.findOne({ email: request.body.email });
-    if (!doc) {
-      throw new Error(serverErrors.guestNotFound);
-    }
-    const code = getRandomWords(4).join('-');
-    const url = new URL(getBaseURL() + '/api/auth/email/check');
-    url.searchParams.set('code', code);
-    const template = handlebars.compile(loginCodeTemplate);
-    const html = template({ code, url: url.href });
-    const transporter = getTransporter();
-    await transporter.sendMail({
-      from: process.env.NODEMAILER_ADDRESS as string,
-      html,
-      subject: 'Your login code is ' + code,
-      to: request.body.email,
-    });
-    const redisClient = await getRedisClient();
-    const redisKey = createRedisKey('codes', code);
-    await redisClient.set(redisKey, doc._id.toString(), { EX: 900 });
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    response.status(202).end();
+  if (!isEmail(request.body.email)) {
+    response.status(400).end();
+    return;
   }
+  const db = await getMongoDatabase();
+  const collection = db.collection('guests');
+  const doc = await collection.findOne({ email: request.body.email });
+  if (!doc) {
+    response.status(400).end();
+    return;
+  }
+  const code = getRandomWords(4).join('-');
+  const url = new URL(getBaseURL() + '/api/auth/email/check');
+  url.searchParams.set('code', code);
+  const template = handlebars.compile(loginCodeTemplate);
+  const html = template({ code, url: url.href });
+  const transporter = getTransporter();
+  await transporter.sendMail({
+    from: process.env.NODEMAILER_ADDRESS as string,
+    html,
+    subject: 'Your login code is ' + code,
+    to: request.body.email,
+  });
+  const redisClient = await getRedisClient();
+  const redisKey = createRedisKey('codes', code);
+  await redisClient.set(redisKey, doc._id.toString(), { EX: 900 });
+  response.status(202).end();
 };
 
 export default applyRateLimiter(handler, {
