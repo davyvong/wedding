acbd1995d831e1cdccdfaf01a7b6c1b9af7c5801
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-03-01 12:19:24 -0500

    refactor: remove server error codes (#5)

--- pages/api/auth/email/check.ts
+++ pages/api/auth/email/check.ts
@@ -1,4 +1,3 @@
-import serverErrors from 'constants/server-errors.json';
 import { serialize } from 'cookie';
 import { ObjectId } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -10,31 +9,28 @@ import { createRedisKey, getRedisClient } from 'utils/redis';
 import { isLoginCode } from 'utils/yup';
 
 const handler = async (request: NextApiRequest, response: NextApiResponse): Promise<void> => {
-  try {
-    if (!isLoginCode(request.query.code)) {
-      throw new Error(serverErrors.invalidLoginCode);
-    }
-    const redisClient = await getRedisClient();
-    const redisKey = createRedisKey('codes', request.query.code as string);
-    const cachedGuestId = await redisClient.get(redisKey);
-    if (!cachedGuestId) {
-      throw new Error(serverErrors.loginCodeNotFound);
-    }
-    const db = await getMongoDatabase();
-    const collection = db.collection('guests');
-    const doc = await collection.findOne({ _id: new ObjectId(cachedGuestId) });
-    if (!doc) {
-      throw new Error(serverErrors.guestNotFound);
-    }
-    await redisClient.del(redisKey);
-    const token = signToken({ id: cachedGuestId });
-    response.setHeader('Set-Cookie', serialize('token', token, { maxAge: 2592000, path: '/' }));
-    response.redirect(getBaseURL());
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    response.status(202).end();
+  if (!isLoginCode(request.query.code)) {
+    response.status(400).end();
+    return;
   }
+  const redisClient = await getRedisClient();
+  const redisKey = createRedisKey('codes', request.query.code as string);
+  const cachedGuestId = await redisClient.get(redisKey);
+  if (!cachedGuestId) {
+    response.status(400).end();
+    return;
+  }
+  const db = await getMongoDatabase();
+  const collection = db.collection('guests');
+  const doc = await collection.findOne({ _id: new ObjectId(cachedGuestId) });
+  if (!doc) {
+    response.status(400).end();
+    return;
+  }
+  await redisClient.del(redisKey);
+  const token = signToken({ id: cachedGuestId });
+  response.setHeader('Set-Cookie', serialize('token', token, { maxAge: 2592000, path: '/' }));
+  response.redirect(getBaseURL());
 };
 
 export default applyRateLimiter(handler);
