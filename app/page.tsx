7dafd926b6d7debc5025746d71e3aa0298e69150
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-03-06 18:17:54 +0000

    feat: add wip title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import classNames from 'classnames';
+import { Fragment } from 'react';
 import type { FC } from 'react';
 
-const Page: FC = () => null;
+import styles from './page.module.css';
+
+const Page: FC = () => (
+  <Fragment>
+    <div className={styles.hero}>
+      <figure className={styles.figure}>
+        <div className={classNames(styles.title, styles.saveTheDate)}>Save The Date</div>
+        <div className={classNames(styles.title, styles.weddingDate)}>June 24, 2024</div>
+      </figure>
+    </div>
+  </Fragment>
+);
 
 export default Page;
