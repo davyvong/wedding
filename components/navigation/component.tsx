c826df0640ce40366dc60dc64121f1a3cf4cbe2b
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-04-17 11:18:20 -0400

    feat: enable navigation in all environments

--- components/navigation/component.tsx
+++ components/navigation/component.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import Link from 'components/link';
 import useTranslate from 'hooks/translate';
-import { Fragment, useMemo } from 'react';
+import { Fragment } from 'react';
 import type { FC, ReactNode } from 'react';
 
 import styles from './component.module.css';
@@ -15,32 +15,23 @@ interface NavigationComponentProps {
 const NavigationComponent: FC<NavigationComponentProps> = ({ children, isOpen, toggle }) => {
   const t = useTranslate();
 
-  const isNavigationEnabled = useMemo(
-    () => process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview',
-    [],
-  );
-
   return (
     <Fragment>
       <div className={classNames(styles.content, isOpen && styles.contentActive)} onClick={isOpen ? toggle : undefined}>
         {children}
       </div>
-      {isNavigationEnabled && (
-        <Fragment>
-          <div className={classNames(styles.menu, isOpen && styles.menuActive)}>
-            <Link href="/" onClick={toggle} text={t('components.navigation.home')} />
-            <Link href="/gallery" onClick={toggle} text={t('components.navigation.gallery')} />
-            <Link href="/cookie-policy" onClick={toggle} text={t('components.navigation.cookie-policy')} />
-          </div>
-          <div className={styles.floatingButton}>
-            <button className={classNames(styles.toggleButton, isOpen && styles.toggleButtonActive)} onClick={toggle}>
-              <span className={styles.toggleButtonLine1} />
-              <span className={styles.toggleButtonLine2} />
-              <span className={styles.toggleButtonLine3} />
-            </button>
-          </div>
-        </Fragment>
-      )}
+      <div className={classNames(styles.menu, isOpen && styles.menuActive)}>
+        <Link href="/" onClick={toggle} text={t('components.navigation.home')} />
+        <Link href="/gallery" onClick={toggle} text={t('components.navigation.gallery')} />
+        <Link href="/cookie-policy" onClick={toggle} text={t('components.navigation.cookie-policy')} />
+      </div>
+      <div className={styles.floatingButton}>
+        <button className={classNames(styles.toggleButton, isOpen && styles.toggleButtonActive)} onClick={toggle}>
+          <span className={styles.toggleButtonLine1} />
+          <span className={styles.toggleButtonLine2} />
+          <span className={styles.toggleButtonLine3} />
+        </button>
+      </div>
     </Fragment>
   );
 };
