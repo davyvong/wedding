da92555a7df2c7f7859302b6c91dea4eca1b39d0
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-06-16 20:54:03 +0000

    feat: add verify token or redirect method

--- server/authenticator.ts
+++ server/authenticator.ts
@@ -1,5 +1,7 @@
 import { JWTPayload } from 'jose';
 import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
+import { redirect } from 'next/navigation';
 import JWT from 'server/jwt';
 
 interface GuestTokenPayload extends JWTPayload {
@@ -7,7 +9,9 @@ interface GuestTokenPayload extends JWTPayload {
 }
 
 class GuestAuthenticator {
-  public static async verifyToken(cookies: RequestCookies): Promise<GuestTokenPayload | undefined> {
+  public static async verifyToken(
+    cookies: RequestCookies | ReadonlyRequestCookies,
+  ): Promise<GuestTokenPayload | undefined> {
     const tokenCookie = cookies.get('token');
     if (!tokenCookie) {
       return undefined;
@@ -17,6 +21,21 @@ class GuestAuthenticator {
       return payload as GuestTokenPayload;
     } catch {
       return undefined;
+    }
+  }
+
+  public static async verifyTokenOrRedirect(
+    cookies: RequestCookies | ReadonlyRequestCookies,
+  ): Promise<GuestTokenPayload | never> {
+    const tokenCookie = cookies.get('token');
+    if (!tokenCookie) {
+      return redirect('/');
+    }
+    try {
+      const payload = await JWT.verify(tokenCookie.value);
+      return payload as GuestTokenPayload;
+    } catch {
+      return redirect('/');
     }
   }
 }
