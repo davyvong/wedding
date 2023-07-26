cf86a4995e995559ae8c71d634ce7fee458cd1fb
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-07-26 18:25:13 +0000

    feat: set all viewport units as properties for styling

--- utils/browser.ts
+++ utils/browser.ts
@@ -1,7 +1,12 @@
 export const isBrowser = (): boolean => typeof window === 'object';
 
-export const setVH = (): void => {
+export const setViewportUnits = (): void => {
   if (isBrowser()) {
-    document.documentElement.style.setProperty('--vh', window.innerHeight * 0.01 + 'px');
+    const vh = window.innerHeight * 0.01;
+    const vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', vh + 'px');
+    document.documentElement.style.setProperty('--vw', vw + 'px');
+    document.documentElement.style.setProperty('--vmax', Math.max(vh, vw) + 'px');
+    document.documentElement.style.setProperty('--vmin', Math.min(vh, vw) + 'px');
   }
 };
