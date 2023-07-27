29f7fee3689c522576300f115d41c9b7b4aeeef8
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-07-26 22:09:02 -0400

    feat: add gsap animation to save the date

--- components/save-the-date/component.tsx
+++ components/save-the-date/component.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import backgroundImage from 'assets/images/VD-72.jpg';
+import coverImageJPG from 'assets/images/VD-72.jpg';
 import classNames from 'classnames';
+import navigationStyles from 'components/navigation/component.module.css';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import useTranslate from 'hooks/translate';
 import localFont from 'next/font/local';
 import Image from 'next/image';
-import { Fragment } from 'react';
+import { useEffect } from 'react';
 import type { FC } from 'react';
 
 import styles from './component.module.css';
@@ -28,25 +31,53 @@ const playfairDislayFont = localFont({
 const SaveTheDateComponent: FC = () => {
   const { t } = useTranslate();
 
-  return (
-    <Fragment>
-      <Image
-        alt={t('components.save-the-date')}
-        className={styles.backgroundImage}
-        fill
-        sizes="(max-width: 425px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, (max-width: 1280px) 100vw, (max-width: 1440px) 100vw, 100vw"
-        src={backgroundImage}
-      />
-      <div className={styles.textOverlay}>
-        <div className={classNames(brittanySignatureFont.className, styles.saveTheDate)}>
-          {t('components.save-the-date')}
-        </div>
-        <div className={classNames(playfairDislayFont.className, styles.names)}>
-          {t('components.save-the-date.names')}
-        </div>
-        <div className={classNames(kollektifFont.className, styles.date)}>{t('components.save-the-date.date')}</div>
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    setTimeout(() => {
+      gsap.to('.' + styles.textMask, {
+        overwrite: true,
+        scrollTrigger: {
+          invalidateOnRefresh: true,
+          pin: true,
+          scroller: document.querySelector('.' + navigationStyles.content),
+          scrub: true,
+          start: 'start start',
+          trigger: '.' + styles.container,
+        },
+        flex: 1,
+      });
+    }, 1000);
+  }, []);
+
+  const TextOverlay = (
+    <div className={styles.textOverlay}>
+      <div className={classNames(brittanySignatureFont.className, styles.saveTheDate)}>
+        {t('components.save-the-date')}
       </div>
-    </Fragment>
+      <div className={classNames(playfairDislayFont.className, styles.names)}>
+        {t('components.save-the-date.names')}
+      </div>
+      <div className={classNames(kollektifFont.className, styles.date)}>{t('components.save-the-date.date')}</div>
+    </div>
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <div className={styles.textMask}>{TextOverlay}</div>
+        <div className={styles.coverImageContainer}>
+          <Image
+            alt={t('components.save-the-date')}
+            className={styles.coverImage}
+            fill
+            priority
+            sizes="(max-width: 425px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, (max-width: 1280px) 100vw, (max-width: 1440px) 100vw, 100vw"
+            src={coverImageJPG}
+          />
+        </div>
+        {TextOverlay}
+      </div>
+    </div>
   );
 };
 
