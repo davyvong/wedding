3ebcbc2b7a5703aafca85c908f27077a14510f15
Davy Vong <24511732+davyvong@users.noreply.github.com>
2023-12-14 20:48:08 +0000

    feat: add table row collapsing animation

--- components/guest-list/component.tsx
+++ components/guest-list/component.tsx
@@ -45,58 +45,68 @@ const GuestListComponent: FC<GuestListComponentProps> = ({ guestGroups }) => {
     return guestGroupClone;
   }, [guestGroups, sortByKey]);
 
-  const renderSeparator = useCallback(
-    (): JSX.Element => (
-      <tr className={styles.guestTableSeparator}>
-        <td colSpan={3}>
-          <hr />
-        </td>
-      </tr>
-    ),
-    [],
-  );
-
   const renderGuestGroup = useCallback(
     (guestGroup, index: number): JSX.Element => (
       <Fragment key={guestGroup.id || index}>
-        {renderSeparator()}
-        <tr
-          className={classNames(
-            styles.guestGroupHeader,
-            collapsedGuestGroups.has(guestGroup.id) && styles.guestGroupHeaderCollapsed,
-          )}
-          onClick={() => {
-            setCollapsedGuestGroups((prevState: Set<string>): Set<string> => {
-              const nextState = new Set(prevState);
-              if (nextState.has(guestGroup.id)) {
-                nextState.delete(guestGroup.id);
-              } else {
-                nextState.add(guestGroup.id);
-              }
-              return nextState;
-            });
-          }}
-        >
-          <td colSpan={3}>
-            <div className={styles.guestGroupHeaderText}>
-              <ExpandLessSVG />
-              {guestGroup.id
-                ? Translate.t('components.guest-list.guest-groups.name', { name: guestGroup.id })
-                : Translate.t('components.guest-list.guest-groups.individual')}
-            </div>
-          </td>
-        </tr>
-        {!collapsedGuestGroups.has(guestGroup.id) &&
-          guestGroup.guests.map(guest => (
-            <tr key={guest.id}>
-              <td>{guest.id}</td>
-              <td>{guest.name}</td>
-              <td>{guest.email}</td>
-            </tr>
-          ))}
+        <tbody>
+          <tr
+            className={classNames(
+              styles.guestGroupHeader,
+              collapsedGuestGroups.has(guestGroup.id) && styles.guestGroupHeaderCollapsed,
+            )}
+            onClick={() => {
+              setCollapsedGuestGroups((prevState: Set<string>): Set<string> => {
+                const nextState = new Set(prevState);
+                if (nextState.has(guestGroup.id)) {
+                  nextState.delete(guestGroup.id);
+                } else {
+                  nextState.add(guestGroup.id);
+                }
+                return nextState;
+              });
+            }}
+          >
+            <td colSpan={3}>
+              <div className={styles.guestGroupHeaderText}>
+                <ExpandLessSVG />
+                {guestGroup.id
+                  ? Translate.t('components.guest-list.guest-groups.name', { name: guestGroup.id })
+                  : Translate.t('components.guest-list.guest-groups.individual')}
+              </div>
+            </td>
+          </tr>
+        </tbody>
+        {guestGroup.guests.length > 0 && (
+          <tbody
+            className={classNames(
+              styles.guestGroupGuests,
+              collapsedGuestGroups.has(guestGroup.id) && styles.guestGroupGuestsCollapsed,
+            )}
+          >
+            {guestGroup.guests.map(guest => (
+              <tr key={guest.id}>
+                <td>
+                  <div>
+                    <div>{guest.id}</div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div>{guest.name}</div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div>{guest.email}</div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        )}
       </Fragment>
     ),
-    [collapsedGuestGroups, renderSeparator],
+    [collapsedGuestGroups],
   );
 
   return (
@@ -109,7 +119,7 @@ const GuestListComponent: FC<GuestListComponentProps> = ({ guestGroups }) => {
             <th>{Translate.t('components.guest-list.guest-table.columns.email')}</th>
           </tr>
         </thead>
-        <tbody>{sortedGuestGroups.map(renderGuestGroup)}</tbody>
+        {sortedGuestGroups.map(renderGuestGroup)}
       </table>
     </div>
   );
