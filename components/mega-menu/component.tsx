b94d5f51f1debcc9dedd8cfa4006f535ff268655
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-02-11 16:49:49 -0500

    feat: add more menu item styling

--- components/mega-menu/component.tsx
+++ components/mega-menu/component.tsx
@@ -13,7 +13,11 @@ import {
   useRole,
   useTransitionStyles,
 } from '@floating-ui/react';
+import MarkEmailReadIconSVG from 'assets/icons/mark-email-read.svg';
+import MarkEmailUnreadIconSVG from 'assets/icons/mark-email-unread.svg';
+import QueueMusicIconSVG from 'assets/icons/queue-music.svg';
 import classNames from 'classnames';
+import Translate from 'client/translate';
 import flyoutStyles from 'components/flyout/component.module.css';
 import InvitationFlyout from 'components/flyouts/invitation';
 import RSVPFlyout from 'components/flyouts/rsvp';
@@ -85,15 +89,39 @@ const MegaMenu: FC<MegaMenuProps> = ({ token }) => {
                 {token ? (
                   <Fragment>
                     <div className={styles.menuItem} onClick={(): void => openFlyout('rsvp')}>
-                      open rsvp
+                      <div className={styles.menuItemIcon}>
+                        <MarkEmailReadIconSVG />
+                      </div>
+                      <div className={styles.menuItemContent}>
+                        <div>{Translate.t('components.flyouts.rsvp.buttons.rsvp')}</div>
+                        <div className={styles.menuItemDescription}>
+                          {Translate.t('components.flyouts.rsvp.buttons.rsvp')}
+                        </div>
+                      </div>
                     </div>
                     <div className={styles.menuItem} onClick={(): void => openFlyout('songs')}>
-                      open songs
+                      <div className={styles.menuItemIcon}>
+                        <QueueMusicIconSVG />
+                      </div>
+                      <div className={styles.menuItemContent}>
+                        <div>{Translate.t('components.flyouts.songs.song-requests')}</div>
+                        <div className={styles.menuItemDescription}>
+                          {Translate.t('components.flyouts.songs.song-requests')}
+                        </div>
+                      </div>
                     </div>
                   </Fragment>
                 ) : (
                   <div className={styles.menuItem} onClick={(): void => openFlyout('invitation')}>
-                    open invitation
+                    <div className={styles.menuItemIcon}>
+                      <MarkEmailUnreadIconSVG />
+                    </div>
+                    <div className={styles.menuItemContent}>
+                      <div>{Translate.t('components.flyouts.invitation.buttons.invitation')}</div>
+                      <div className={styles.menuItemDescription}>
+                        {Translate.t('components.flyouts.invitation.buttons.invitation')}
+                      </div>
+                    </div>
                   </div>
                 )}
               </div>
