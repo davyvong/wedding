00c125695bbb3b8b5778ac8caffa991fc0ebbdbc
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-02-12 01:12:03 +0000

    feat: remove blur overlay from mega menu

--- components/mega-menu/component.tsx
+++ components/mega-menu/component.tsx
@@ -2,7 +2,6 @@
 
 import {
   FloatingFocusManager,
-  FloatingOverlay,
   autoUpdate,
   offset,
   shift,
@@ -18,7 +17,6 @@ import MarkEmailUnreadIconSVG from 'assets/icons/mark-email-unread.svg';
 import QueueMusicIconSVG from 'assets/icons/queue-music.svg';
 import classNames from 'classnames';
 import Translate from 'client/translate';
-import flyoutStyles from 'components/flyout/component.module.css';
 import InvitationFlyout from 'components/flyouts/invitation';
 import RSVPFlyout from 'components/flyouts/rsvp';
 import SongsFlyout from 'components/flyouts/songs';
@@ -36,10 +34,10 @@ const MegaMenu: FC<MegaMenuProps> = ({ token }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const { context, floatingStyles, refs } = useFloating({
-    middleware: [offset(8), shift({ padding: 24 })],
+    middleware: [offset(24), shift({ padding: 24 })],
     onOpenChange: setIsOpen,
     open: isOpen,
-    placement: 'bottom',
+    placement: 'bottom-end',
     whileElementsMounted: autoUpdate,
   });
 
@@ -47,9 +45,6 @@ const MegaMenu: FC<MegaMenuProps> = ({ token }) => {
   const dismiss = useDismiss(context);
   const role = useRole(context);
   const { isMounted, styles: transitionStyles } = useTransitionStyles(context, {
-    duration: 300,
-  });
-  const { styles: overlayStyles } = useTransitionStyles(context, {
     duration: 300,
   });
 
@@ -75,58 +70,53 @@ const MegaMenu: FC<MegaMenuProps> = ({ token }) => {
       </button>
       {isMounted &&
         createPortal(
-          <Fragment>
-            <FloatingOverlay className={flyoutStyles.overlay} lockScroll>
-              <div className={flyoutStyles.overlayBlur} style={overlayStyles} />
-            </FloatingOverlay>
-            <FloatingFocusManager context={context}>
-              <div
-                {...getFloatingProps()}
-                className={styles.megaMenu}
-                ref={refs.setFloating}
-                style={{ ...floatingStyles, ...transitionStyles }}
-              >
-                {token ? (
-                  <Fragment>
-                    <div className={styles.menuItem} onClick={(): void => openFlyout('rsvp')}>
-                      <div className={styles.menuItemIcon}>
-                        <MarkEmailReadIconSVG />
-                      </div>
-                      <div className={styles.menuItemContent}>
-                        <div>{Translate.t('components.flyouts.rsvp.buttons.rsvp')}</div>
-                        <div className={styles.menuItemDescription}>
-                          {Translate.t('components.flyouts.rsvp.buttons.rsvp')}
-                        </div>
-                      </div>
-                    </div>
-                    <div className={styles.menuItem} onClick={(): void => openFlyout('songs')}>
-                      <div className={styles.menuItemIcon}>
-                        <QueueMusicIconSVG />
-                      </div>
-                      <div className={styles.menuItemContent}>
-                        <div>{Translate.t('components.flyouts.songs.song-requests')}</div>
-                        <div className={styles.menuItemDescription}>
-                          {Translate.t('components.flyouts.songs.song-requests')}
-                        </div>
-                      </div>
-                    </div>
-                  </Fragment>
-                ) : (
-                  <div className={styles.menuItem} onClick={(): void => openFlyout('invitation')}>
+          <FloatingFocusManager context={context}>
+            <div
+              {...getFloatingProps()}
+              className={styles.megaMenu}
+              ref={refs.setFloating}
+              style={{ ...floatingStyles, ...transitionStyles }}
+            >
+              {token ? (
+                <Fragment>
+                  <div className={styles.menuItem} onClick={(): void => openFlyout('rsvp')}>
                     <div className={styles.menuItemIcon}>
-                      <MarkEmailUnreadIconSVG />
+                      <MarkEmailReadIconSVG />
                     </div>
                     <div className={styles.menuItemContent}>
-                      <div>{Translate.t('components.flyouts.invitation.buttons.invitation')}</div>
+                      <div>{Translate.t('components.flyouts.rsvp.buttons.rsvp')}</div>
                       <div className={styles.menuItemDescription}>
-                        {Translate.t('components.flyouts.invitation.buttons.invitation')}
+                        {Translate.t('components.flyouts.rsvp.buttons.rsvp')}
                       </div>
                     </div>
                   </div>
-                )}
-              </div>
-            </FloatingFocusManager>
-          </Fragment>,
+                  <div className={styles.menuItem} onClick={(): void => openFlyout('songs')}>
+                    <div className={styles.menuItemIcon}>
+                      <QueueMusicIconSVG />
+                    </div>
+                    <div className={styles.menuItemContent}>
+                      <div>{Translate.t('components.flyouts.songs.song-requests')}</div>
+                      <div className={styles.menuItemDescription}>
+                        {Translate.t('components.flyouts.songs.song-requests')}
+                      </div>
+                    </div>
+                  </div>
+                </Fragment>
+              ) : (
+                <div className={styles.menuItem} onClick={(): void => openFlyout('invitation')}>
+                  <div className={styles.menuItemIcon}>
+                    <MarkEmailUnreadIconSVG />
+                  </div>
+                  <div className={styles.menuItemContent}>
+                    <div>{Translate.t('components.flyouts.invitation.buttons.invitation')}</div>
+                    <div className={styles.menuItemDescription}>
+                      {Translate.t('components.flyouts.invitation.buttons.invitation')}
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </FloatingFocusManager>,
           document.body,
         )}
       {token ? (
