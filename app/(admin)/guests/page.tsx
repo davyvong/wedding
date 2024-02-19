06b6c3115000a5611b27ba215c850b81221393a9
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-02-19 16:37:34 -0500

    feat: disable cache on guests page

--- app/(admin)/guests/page.tsx
+++ app/(admin)/guests/page.tsx
@@ -1,9 +1,5 @@
 import GuestList from 'components/guest-list';
 import { FC } from 'react';
-import RedisClientFactory from 'server/clients/redis';
-import { GuestData } from 'server/models/guest';
-import RedisKey from 'server/models/redis-key';
-import { ResponseData } from 'server/models/response';
 import MySQLQueries from 'server/queries/mysql';
 import { generateDefaultMetadata } from 'utils/metadata';
 
@@ -13,22 +9,12 @@ export const metadata = generateDefaultMetadata({
 export const runtime = 'edge';
 
 const Page: FC = async () => {
-  const redisClient = RedisClientFactory.getInstance();
-  const redisKey = RedisKey.create('guest-list');
-  const cachedGuestList =
-    await redisClient.get<{ guests: GuestData[]; id: string; responses: ResponseData[] }[]>(redisKey);
-
-  if (cachedGuestList) {
-    return <GuestList guestList={cachedGuestList} />;
-  }
-
   const guestList = await MySQLQueries.findGuestList();
   const guestListData = guestList.map(guestGroup => ({
     guests: guestGroup.guests.map(guest => guest.valueOf()),
     id: guestGroup.id,
     responses: guestGroup.responses.map(response => response.valueOf()),
   }));
-  await redisClient.set(redisKey, guestListData, { ex: 60 });
 
   return <GuestList guestList={guestListData} />;
 };
