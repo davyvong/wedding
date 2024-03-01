b98663f1427dcc4ae1d22d56667ee619577ec351
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-02-29 23:22:07 -0500

    feat: add dress code to faq

--- components/frequently-asked-questions/constants.ts
+++ components/frequently-asked-questions/constants.ts
@@ -5,19 +5,19 @@ export interface FrequentlyAskedQuestion {
 
 export const frequentlyAskedQuestions: FrequentlyAskedQuestion[] = [
   {
-    answer: 'components.frequently-asked-questions.answers.where-is-the-ceremoney-and-reception',
-    question: 'components.frequently-asked-questions.questions.where-is-the-ceremoney-and-reception',
+    answer: 'components.frequently-asked-questions.answers.what-is-the-dress-code',
+    question: 'components.frequently-asked-questions.questions.what-is-the-dress-code',
   },
   {
-    answer: 'components.frequently-asked-questions.answers.i-did-not-receive-an-email',
-    question: 'components.frequently-asked-questions.questions.i-did-not-receive-an-email',
+    answer: 'components.frequently-asked-questions.answers.how-to-respond-to-invitation',
+    question: 'components.frequently-asked-questions.questions.how-to-respond-to-invitation',
   },
   {
     answer: 'components.frequently-asked-questions.answers.when-is-the-rsvp-deadline',
     question: 'components.frequently-asked-questions.questions.when-is-the-rsvp-deadline',
   },
   {
-    answer: 'components.frequently-asked-questions.answers.how-to-respond-to-invitation',
-    question: 'components.frequently-asked-questions.questions.how-to-respond-to-invitation',
+    answer: 'components.frequently-asked-questions.answers.i-did-not-receive-an-email',
+    question: 'components.frequently-asked-questions.questions.i-did-not-receive-an-email',
   },
 ];
