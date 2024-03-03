a6064f8b12368f457bcfbf24b790348b034cf781
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-03-02 22:00:46 -0500

    feat: add event details for unauthenticated guests

--- components/event-details/component.tsx
+++ components/event-details/component.tsx
@@ -1,9 +1,10 @@
 import EventIconSVG from 'assets/icons/event.svg';
+import LockIconSVG from 'assets/icons/lock.svg';
 import MapIconSVG from 'assets/icons/map.svg';
 import classNames from 'classnames';
 import Translate from 'client/translate';
 import faqStyles from 'components/frequently-asked-questions/component.module.css';
-import { FC, Fragment } from 'react';
+import { FC } from 'react';
 import { VerifiedGuestTokenPayload } from 'server/authenticator';
 
 import styles from './component.module.css';
@@ -14,7 +15,16 @@ export interface EventDetailsComponentProps {
 
 const EventDetailsComponent: FC<EventDetailsComponentProps> = ({ token }) => {
   if (!token) {
-    return <Fragment />;
+    return (
+      <div className={faqStyles.faq}>
+        <div className={faqStyles.faqTitle}>{Translate.t('components.event-details.title')}</div>
+        <div className={classNames(faqStyles.faqQuestion, styles.header)}>
+          <LockIconSVG />
+          <span>{Translate.t('components.event-details.headers.locked')}</span>
+        </div>
+        <div className={faqStyles.faqAnswer}>{Translate.html('components.event-details.descriptions.locked')}</div>
+      </div>
+    );
   }
 
   return (
@@ -22,16 +32,16 @@ const EventDetailsComponent: FC<EventDetailsComponentProps> = ({ token }) => {
       <div className={faqStyles.faqTitle}>{Translate.t('components.event-details.title')}</div>
       <div className={classNames(faqStyles.faqQuestion, styles.header)}>
         <EventIconSVG />
-        <span>{Translate.t('components.event-details.date')}</span>
+        <span>{Translate.t('components.event-details.headers.date')}</span>
       </div>
-      <div className={faqStyles.faqAnswer}>{Translate.t('components.event-details.ceremony-time')}</div>
+      <div className={faqStyles.faqAnswer}>{Translate.t('components.event-details.descriptions.ceremony')}</div>
       <div className={classNames(faqStyles.faqQuestion, styles.header)}>
         <MapIconSVG />
-        <span>{Translate.t('components.event-details.venue')}</span>
+        <span>{Translate.t('components.event-details.headers.venue')}</span>
       </div>
       <div className={faqStyles.faqAnswer}>
         <a href="https://maps.app.goo.gl/qwB4NwCU742cEDp36" target="_blank">
-          {Translate.t('components.event-details.venue-address')}
+          {Translate.t('components.event-details.descriptions.address')}
         </a>
       </div>
     </div>
