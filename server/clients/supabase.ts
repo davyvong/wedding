888a3282055cbf32c61bbae2262d32f5cc768fbd
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-03-09 00:01:00 -0500

    chore: add supabase service key

--- server/clients/supabase.ts
+++ server/clients/supabase.ts
@@ -2,7 +2,7 @@ import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { Database } from 'types/supabase';
 
 class SupabaseClientFactory {
-  private static readonly instance = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+  private static readonly instance = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
   public static getInstance(): SupabaseClient {
     return SupabaseClientFactory.instance;
