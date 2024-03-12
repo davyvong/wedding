7573920d78f0d2ded9c6839d74a9f6b1894837c7
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-03-11 20:29:32 -0400

    chore: add cloudflare environment variables

--- server/apis/cloudflare.ts
+++ server/apis/cloudflare.ts
@@ -1,24 +1,19 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const ACCOUNT_ID = '';
-const ACCESS_KEY_ID = '';
-const SECRET_ACCESS_KEY = '';
-const BUCKET_ID = '';
-
 class CloudflareAPI {
   private static s3Client = new S3Client({
     credentials: {
-      accessKeyId: ACCESS_KEY_ID,
-      secretAccessKey: SECRET_ACCESS_KEY,
+      accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID,
+      secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY,
     },
-    endpoint: new URL('https://' + ACCOUNT_ID + '.r2.cloudflarestorage.com/').href,
+    endpoint: new URL('https://' + process.env.CLOUDFLARE_ACCOUNT_ID + '.r2.cloudflarestorage.com/').href,
     region: 'auto',
   });
 
   public static async getSignedUrl(key: string): Promise<URL> {
     const command = new PutObjectCommand({
-      Bucket: BUCKET_ID,
+      Bucket: process.env.CLOUDFLARE_BUCKET_ID,
       Key: key,
     });
     const signedURL = await getSignedUrl(CloudflareAPI.s3Client, command, {
