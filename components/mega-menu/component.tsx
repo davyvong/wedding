784da4f291186f08f9091507dacd63c71e18ca86
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-05-06 21:20:18 -0400

    feat: enable scavenger hunt menu item on june 20

--- components/mega-menu/component.tsx
+++ components/mega-menu/component.tsx
@@ -129,29 +129,29 @@ const MegaMenu: FC<MegaMenuProps> = ({ token }) => {
           type: 'button',
         },
       );
+      if (new Date().toISOString() > '2024-06-20T00:00:00.000Z') {
+        items.push({
+          description: Translate.t('components.mega-menu.menu-items.scavenger-hunt.description'),
+          href: '/scavenger',
+          icon: <ContentPasteSearchIconSVG />,
+          onClick: (): void => {
+            setIsOpen(false);
+          },
+          title: Translate.t('components.mega-menu.menu-items.scavenger-hunt.title'),
+          type: 'link',
+        });
+      }
       if (token.isAdmin) {
-        items.push(
-          {
-            description: Translate.t('components.mega-menu.menu-items.scavenger-hunt.description'),
-            href: '/scavenger',
-            icon: <ContentPasteSearchIconSVG />,
-            onClick: (): void => {
-              setIsOpen(false);
-            },
-            title: Translate.t('components.mega-menu.menu-items.scavenger-hunt.title'),
-            type: 'link',
+        items.push({
+          description: Translate.t('components.mega-menu.menu-items.scavenger-hunt-submissions.description'),
+          href: '/scavenger/submissions',
+          icon: <ContentPasteSearchIconSVG />,
+          onClick: (): void => {
+            setIsOpen(false);
           },
-          {
-            description: Translate.t('components.mega-menu.menu-items.scavenger-hunt-submissions.description'),
-            href: '/scavenger/submissions',
-            icon: <ContentPasteSearchIconSVG />,
-            onClick: (): void => {
-              setIsOpen(false);
-            },
-            title: Translate.t('components.mega-menu.menu-items.scavenger-hunt-submissions.title'),
-            type: 'link',
-          },
-        );
+          title: Translate.t('components.mega-menu.menu-items.scavenger-hunt-submissions.title'),
+          type: 'link',
+        });
       }
     } else {
       items.push({
