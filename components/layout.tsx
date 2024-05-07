cbfbf8ecee64bcc0910cd41dd2af40c0b9029d4d
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-05-06 20:51:18 -0400

    feat: disable input zoom on ios safari

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import 'minireset.css';
 
 import '../app/global.css';
@@ -5,7 +7,7 @@ import '../app/global.css';
 import { Analytics } from '@vercel/analytics/react';
 import { openSans } from 'client/fonts';
 import NavigationBar from 'components/navigation-bar';
-import type { FC, ReactNode } from 'react';
+import { useEffect, type FC, type ReactNode } from 'react';
 import { VerifiedGuestTokenPayload } from 'server/authenticator';
 
 interface LayoutProps {
@@ -13,14 +15,27 @@ interface LayoutProps {
   token?: VerifiedGuestTokenPayload;
 }
 
-const Layout: FC<LayoutProps> = ({ children, token }) => (
-  <html lang="en">
-    <body className={openSans.className}>
-      <NavigationBar token={token} />
-      {children}
-      <Analytics />
-    </body>
-  </html>
-);
+const Layout: FC<LayoutProps> = ({ children, token }) => {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && navigator.userAgent.indexOf('iPhone') > 1) {
+      document
+        .querySelector('[name=viewport]')
+        ?.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1');
+    }
+  }, []);
+
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={openSans.className}>
+        <NavigationBar token={token} />
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+};
 
 export default Layout;
