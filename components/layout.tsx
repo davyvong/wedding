8da579d96ba27e543d7d3f88dec4c7bc4b7e4ea4
Davy Vong <24511732+davyvong@users.noreply.github.com>
2024-05-13 21:13:46 -0400

    chore: remove duplicate viewport and vercel analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,6 @@ import 'minireset.css';
 
 import '../app/global.css';
 
-import { Analytics } from '@vercel/analytics/react';
 import { openSans } from 'client/fonts';
 import NavigationBar from 'components/navigation-bar';
 import { type FC, type ReactNode } from 'react';
@@ -16,13 +15,9 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children, token }) => (
   <html lang="en">
-    <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
     <body className={openSans.className}>
       <NavigationBar token={token} />
       {children}
-      <Analytics />
       <Viewport />
     </body>
   </html>
